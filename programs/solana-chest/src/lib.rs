5057f1e013acbbfa53f004c72c64a95d83d3cb7b
Iceomatic <89707822+iceomatic@users.noreply.github.com>
2022-12-15 02:07:05 +0100

    fix: createATA now working

--- programs/solana-chest/src/lib.rs
+++ programs/solana-chest/src/lib.rs
@@ -1,88 +1,89 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token::{Mint, Token, TokenAccount};
+use anchor_spl::associated_token::{self, AssociatedToken, Create};
 
 // This is your program's public key and it will update
 // automatically when you build the project.
 declare_id!("9JsNvQjgaPovfyYy62vcm17HFCczsfBURvnXDQQrHUuB");
 
 #[program]
-pub mod solana_chest {
+mod solana_chest {
     use super::*;
     pub fn create_chest(ctx: Context<CreateChest>) -> Result<()> {
         let nft_token_account = &ctx.accounts.nft_token_account;
         let nft_mint = &ctx.accounts.nft_mint;
         let user = &ctx.accounts.user;
-
+        
         require_eq!(nft_token_account.owner, user.key(), MyError::TokenOwnership);
         assert_eq!(nft_token_account.mint, nft_mint.key());
         require_eq!(nft_token_account.amount, 1, MyError::TokenFungible);
+        
 
         Ok(())
     }
 
-    pub fn put_into_chest(ctx: Context<PutIntoChest>) -> Result<()> {
-        let sender: &Signer = &ctx.accounts.sender; // Human Controlled Account
-        let chest: &Account<TokenAccount> = &ctx.accounts.chest; // this is the NFT that is a chest: 'owns' other NFTs
-        let chest_pda = &ctx.accounts.chest_pda; // PDA derived from this smart contract and Human Controlled Account. Just an implementation detail to sign txs TODO: anchor constraint to ensure it derives from chest
-        let thing_associated_account: &Account<TokenAccount> =
-            &ctx.accounts.thing_associated_account; // an associated token account created to hold the 'thing' under chest_pda.
+    pub fn create_ata(ctx: Context<CreateATA>) -> Result<()> {
+        let cpi_accounts = Create {
+            payer: ctx.accounts.payer.to_account_info(),
+            associated_token: ctx.accounts.new_token_account.to_account_info(),
+            authority: ctx.accounts.chest_pda.to_account_info(),
+            mint: ctx.accounts.mint.to_account_info(),
+            system_program: ctx.accounts.system_program.to_account_info(),
+            token_program: ctx.accounts.token_program.to_account_info(),
+            rent: ctx.accounts.rent.to_account_info(),
+        };
 
-        anchor_spl::token::transfer(
-            CpiContext::new(
-                // a human signs this so we use new() instead of new_with_signer()
-                ctx.accounts.token_program.to_account_info(),
-                anchor_spl::token::Transfer {
-                    from: sender.to_account_info(), // user must already own the NFTs, TODO: anchor constraint
-                    to: chest_pda.to_account_info(),
-                    authority: sender.to_account_info(),
-                },
-            ),
-            1,
-        )
+        let cpi_program = ctx.accounts.associated_token_program.to_account_info();
+
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        associated_token::create(cpi_ctx)
     }
 }
+
 
 #[derive(Accounts)]
 pub struct CreateChest<'info> {
     // We must specify the space in order to initialize an account.
     // First 8 bytes are default account discriminator,
     // (u64 = 64 bits unsigned integer = 8 bytes)
-    #[account(init, seeds = [b"chest", nft_token_account.mint.as_ref()], bump, payer = user, space = 8)]
-    pub new_account: Account<'info, ChestPDA>,
+    #[account(init, seeds = [b"chest", nft_token_account.mint.as_ref()], bump, payer = user, space=8+8)]
+    pub new_account: Account<'info, ChestPda>,
+
     #[account(mut)]
     pub user: Signer<'info>,
-
+    
     pub system_program: Program<'info, System>,
     #[account()]
     pub nft_mint: Account<'info, Mint>,
     #[account()]
     pub nft_token_account: Account<'info, TokenAccount>,
+
+
 }
 
 #[derive(Accounts)]
-pub struct PutIntoChest<'info> {
-    #[account(mut)]
-    pub sender: Signer<'info>, // HUMAN CONTROLLED ACCOUNT
-    #[account(mut)]
-    pub chest: Account<'info, TokenAccount>, // The NFT that was converted into a Chest. SPL NFT derived from SENDER and NFT_MINT
-    #[account(mut)]
-    pub chest_pda: Account<'info, ChestPDA>, // the PDA that can sign for the 'thing' to be transferred elsewhere
+pub struct CreateATA<'info> {
 
-    #[account(init_if_needed, payer = sender, associated_token::mint = mint, associated_token::authority = sender)]
-    pub thing_associated_account: Account<'info, TokenAccount>, // derived from chest_pda and the thing's NFT mint
+    #[account(mut)]
+    pub payer: Signer<'info>,
 
-    pub mint: Account<'info, Mint>, // Mint of transfered NFT
+    #[account(mut)]
+    ///CHECK maybe
+    pub new_token_account: UncheckedAccount<'info>,
+    
 
-    #[account(address = SYSTEM_PROGRAM_ID)]
-    pub system_program: Program<'info, System>,
-    #[account(address = TOKEN_PROGRAM_ID)]
+    pub chest_pda: Account<'info,ChestPda>,
+    pub mint: Account<'info, Mint>, // Mint addy of token
+    pub token_account: Account<'info, TokenAccount>,
     pub token_program: Program<'info, Token>,
-    #[account(address = ASSOCIATED_TOKEN_PROGRAM_ID)]
-    pub associated_token_program: Program<'info, anchor_spl::associated_token::AssociatedToken>,
+    pub system_program: Program<'info, System>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
     pub rent: Sysvar<'info, Rent>,
 }
+
 #[account]
-pub struct ChestPDA {}
+pub struct ChestPda {
+}
 
 #[error_code]
 pub enum MyError {
@@ -91,5 +92,6 @@ pub enum MyError {
     #[msg(Token not owned by signer)]
     TokenOwnership,
     #[msg(Token should be non-fungible)]
-    TokenFungible,
+    TokenFungible
 }
+
--- tests/solana-chest.ts
+++ tests/solana-chest.ts
@@ -2,82 +2,183 @@ import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { SolanaChest } from "../target/types/solana_chest";
 import * as web3 from "@solana/web3.js";
+import * as fs from "fs";
+import {getAccount, createMint, mintTo, getOrCreateAssociatedTokenAccount, createSetAuthorityInstruction, AuthorityType, getMint} from "@solana/spl-token";
+import { assert } from "console";
+import { token } from "@project-serum/anchor/dist/cjs/utils";
 
-describe("solana-chest", () => {
+describe("solana_chest", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.SolanaChest as Program<SolanaChest>;
+  const connection = new web3.Connection("http://127.0.0.1:8899", "confirmed");
 
-  it("Is initialized!", async () => {
-    /*
+  it("Create Token & Create Chest PDA", async () => {
     // Generate keypair for the new account
+    const rawPayerKeypair = JSON.parse(fs.readFileSync("testuser.json", "utf-8"));
+    const test_user = web3.Keypair.fromSecretKey(Buffer.from(rawPayerKeypair));
+
     const mint = await createMint(
-    program.connection,
-    program.wallet.keypair,
-    program.wallet.publicKey,
-    program.wallet.publicKey,
+    connection,
+    test_user,
+    test_user.publicKey,
+    test_user.publicKey,
     0
     );
-
     const associatedTokenAccount = await getOrCreateAssociatedTokenAccount(
-    program.connection,
-    program.wallet.keypair,
+    connection,
+    test_user,
     mint,
-    program.wallet.publicKey
+    test_user.publicKey
   );
-
     await mintTo(
-    program.connection,
-    program.wallet.keypair,
+    connection,
+    test_user,
     mint,
     associatedTokenAccount.address,
-    program.wallet.keypair,
+    test_user,
     1
   );
-  let transaction = new Transaction()
+  let transaction = new web3.Transaction()
   .add(createSetAuthorityInstruction(
     mint,
-    program.wallet.publicKey,
+    test_user.publicKey,
     AuthorityType.MintTokens,
     null
   ));
-
-await web3.sendAndConfirmTransaction(program.connection, transaction, [program.wallet.keypair]);
-const accountInfo = await getAccount(program.connection, associatedTokenAccount.address);
-
-console.log(await accountInfo.amount);
+await web3.sendAndConfirmTransaction(connection, transaction, [test_user]);
+const accountInfo = await getAccount(connection, associatedTokenAccount.address);
+console.log("Token Account:" + accountInfo.address.toString());
+console.log("Token Amount:" + accountInfo.amount.toString());
 // 1
 const mintInfo = await getMint(
-    program.connection,
+    connection,
     mint
   );
-
-console.log(await mintInfo);
+  console.log("Mint Address:" + mintInfo.address.toString());
     // Send transaction
-  });
-  */
-    const token_mint = new web3.PublicKey("GrtCUVQSKuwhtULMgopacQdg69tvqvF24mXZuN4ScsyR")
-    const token_address = new web3.PublicKey("EZ6Vm4sHjXiiMQT4a7nd5Hgnra2YSS5LjK8BXj6MfJ75")
-    const [newPDA] = await web3.PublicKey.findProgramAddress([
+
+    const token_mint = mintInfo.address;
+    const token_address = accountInfo.address;
+    const [newPDA] = await web3.PublicKey.findProgramAddressSync([
       anchor.utils.bytes.utf8.encode("chest"),
       token_mint.toBuffer()
     ],
       program.programId)
 
-    console.log(newPDA.toString());
+    console.log("PDA being created: " + newPDA.toString());
 
     const tx = await program.methods.createChest().accounts(
       {
         nftMint: token_mint,
         nftTokenAccount: token_address,
         newAccount: newPDA,
-        user: program.wallet.publicKey,
+        user: test_user.publicKey,
         systemProgram: web3.SystemProgram.programId
       }
     ).signers(
-      [program.wallet.keypair]
+      [test_user]
     ).rpc()
-    console.log(tx)
+    console.log("TX: " + tx.toString())
+
+    const pda_info = await program.account.chestPda.getAccountInfo(newPDA);
+    console.log(pda_info);
+    assert(pda_info.lamports > 0, "Account initialized")
   });
+
+ it("Create ATA for PDA", async () => {
+
+    const rawPayerKeypair = JSON.parse(fs.readFileSync("testuser.json", "utf-8"));
+    const test_user = web3.Keypair.fromSecretKey(Buffer.from(rawPayerKeypair));
+
+    const mint = await createMint(
+    connection,
+    test_user,
+    test_user.publicKey,
+    test_user.publicKey,
+    0
+    );
+    const associatedTokenAccount = await getOrCreateAssociatedTokenAccount(
+    connection,
+    test_user,
+    mint,
+    test_user.publicKey
+  );
+    await mintTo(
+    connection,
+    test_user,
+    mint,
+    associatedTokenAccount.address,
+    test_user,
+    1
+  );
+  let transaction = new web3.Transaction()
+  .add(createSetAuthorityInstruction(
+    mint,
+    test_user.publicKey,
+    AuthorityType.MintTokens,
+    null
+  ));
+await web3.sendAndConfirmTransaction(connection, transaction, [test_user]);
+const accountInfo = await getAccount(connection, associatedTokenAccount.address);
+console.log("Token Account:" + accountInfo.address.toString());
+console.log("Token Amount:" + accountInfo.amount.toString());
+// 1
+const mintInfo = await getMint(
+    connection,
+    mint
+  );
+  console.log("Mint Address:" + mintInfo.address.toString());
+    // Send transaction
+
+    const token_mint = mintInfo.address;
+    const token_address = accountInfo.address;
+    const [newPDA] = await web3.PublicKey.findProgramAddressSync([
+      anchor.utils.bytes.utf8.encode("chest"),
+      token_mint.toBuffer()
+    ],
+      program.programId)
+
+    console.log("PDA being created: " + newPDA.toString());
+
+    const tx = await program.methods.createChest().accounts(
+      {
+        nftMint: token_mint,
+        nftTokenAccount: token_address,
+        newAccount: newPDA,
+        user: test_user.publicKey,
+        systemProgram: web3.SystemProgram.programId
+      }
+    ).signers(
+      [test_user]
+    ).rpc()
+    console.log("TX: " + tx.toString())
+    
+    const token_program_id = new web3.PublicKey("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA");
+    const at_program = new web3.PublicKey("ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL");
+
+    const [new_token_account] = web3.PublicKey.findProgramAddressSync([newPDA.toBytes(),token_program_id.toBytes(), token_mint.toBytes()],at_program)
+
+    const tx_ata = await program.methods.createAta().accounts(
+      {
+        payer: test_user.publicKey,
+        newTokenAccount: new_token_account,
+        chestPda: newPDA,
+        mint: token_mint,
+        tokenAccount: token_address,
+        tokenProgram: token_program_id,
+        systemProgram: web3.SystemProgram.programId,
+        associatedTokenProgram: at_program,
+        rent: web3.SYSVAR_RENT_PUBKEY
+        
+      }
+    ).signers([
+      test_user
+    ]).rpc()
+    console.log("TX: " + tx_ata.toString())
+    await connection.confirmTransaction(tx)
+    console.log(await connection.getAccountInfo(new_token_account))
+  })
+
 });
